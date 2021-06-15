2372da11cec41990d7d7ac4dc21da2bf6bce64ef
AbbasMansoori <abbas.mansoori@iths.se>
2021-06-15 18:56:27 +0200

    implemented messageListScreen and other things

--- src/screens/MessageListScreen.js
+++ src/screens/MessageListScreen.js
@@ -1,36 +1,78 @@
 import { LinearGradient } from "expo-linear-gradient";
-import React from "react";
-import { View, SafeAreaView, Text, Button, StyleSheet } from "react-native";
-import FlatList from "../components/FlatListComponent";
+import React, { useEffect, useState} from "react";
+import {
+  FlatList,
+  View, 
+  SafeAreaView, 
+  Text, 
+  Button
+} from "react-native";
+
+
 import { getMessage } from "../api/authService";
 
-const s = require("../style/style");
+const styles = require("../style/style");
 
 export default function MessageListScreen() {
- 
 
-  
-  
-  return (
-    <SafeAreaView>
-      <LinearGradient colors={['#4A148C', '#880E4F']}
-          style={s.container}>
-      <View >
-        <Text style={s.text}>Here are Lists of your Messages!</Text>
-        <FlatList />
-        <Button title="Press me" onPress={getMessage}></Button>
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    const initMessages = async () =>{
+      const response = await getMessage();
+      console.log(response)
+      setMessages([...response.messages])
+    }
+    initMessages()
+  },[])
+
+
+  const Item = ({title, content}) => {
+    return(
+      <View>
+        <Text style={styles.text}>{title}</Text>
+        <Text style={styles.text}>{content}</Text>
+
       </View>
-  </LinearGradient>
+
+
+    )
+  }
+
+  const renderMessages = ({item}) =>(
+
+    <Item title={`Title: ${item.title}`} content={`Content: ${item.content}`} />
+
+  )
+
+
+
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <LinearGradient colors={["#4A148C", "#880E4F"]} style={styles.container}>
+        <View>
+          <Text style={styles.text}>Here are your Messages!</Text>
+          <FlatList
+          data={messages}
+          renderItem={renderMessages}
+          keyExtractor={(item) => item.id}
+          
+          />
+          {/* <Button title="Press me" onPress={initMessages}></Button> */}
+        </View>
+      </LinearGradient>
     </SafeAreaView>
   );
 }
 
 
-// const MessageListScreen = (props) => {
-//   const pressHandler = () => {
-//     props.navigation.navigate("TestScreen");
-//   };
-// };
+
+
+
+
+
 
 
 
--- src/screens/TaskListScreen.js
+++ src/screens/TaskListScreen.js
@@ -1,24 +1,63 @@
-import React from "react";
-import { View, SafeAreaView, Text, Button, StyleSheet } from "react-native";
-import FlatList from "../components/FlatListComponent";
-const s = require("../style/style");
 import { LinearGradient } from "expo-linear-gradient";
+import React, { useEffect, useState} from "react";
+import {
+  FlatList,
+  SafeAreaView,
+  View,
+  Text,
+} from "react-native";
 
-const TaskListScreen = (props) => {
-  const pressHandler = () => {
-    props.navigation.navigate("TestScreen");
+
+const styles = require("../style/style");
+
+import { getClientTask } from "../api/authService";
+
+
+export default function TestScreen() {
+
+  
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const initTasks = async () => {
+      const res = await getClientTask();
+      console.log(res);
+      setTasks([...res.task]);
+    };
+    initTasks();
+  }, []);
+
+
+
+  const Item = ({ title, done }) => {
+    return (
+      <View style={styles.listItems}>
+        <Text style={styles.text}>{title}</Text>
+        <Text style={styles.text}>{done}</Text>
+      </View>
+    );
   };
+
+
+
+  const renderItem = ({ item }) => (
+    <Item title={item.title} done={"Done: " + item.done} />
+  );
+
+
+
   return (
-    <SafeAreaView>
-      <LinearGradient colors={["#4A148C", "#880E4F"]} style={s.container}>
-        <View>
-          <Text style={s.heading}>Here are Lists of your previous Tasks</Text>
-          <FlatList />
-          <Button onPress={pressHandler} title="List Tasks" />
-        </View>
+    <SafeAreaView style={styles.container}>
+      <LinearGradient colors={["#4A148C", "#880E4F"]} style={styles.container}>
+      <FlatList
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
       </LinearGradient>
     </SafeAreaView>
   );
-};
+}
 
-export default TaskListScreen;
+
+
